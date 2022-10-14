d3f99ce44b33cc48e1a85477c2e6fc2cc8f088ba
swen0001 <oleksandr.venger0001@gmail.com>
2022-10-14 16:39:09 +0300

    User Views

--- users/serializers.py
+++ users/serializers.py
@@ -2,7 +2,23 @@ from rest_framework import serializers
 from .models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
+class RelatedUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        exclude = ('id',
+                   'email',
+                   'groups',
+                   'user_permissions',
+                   'password',
+                   'last_login',
+                   'is_superuser',
+                   'is_staff',
+                   'is_active',
+                   'date_joined',
+                   'favs',)
+
+
+class ReadUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         exclude = ('groups',
@@ -12,5 +28,4 @@ class UserSerializer(serializers.ModelSerializer):
                    'is_superuser',
                    'is_staff',
                    'is_active',
-                   'date_joined',
-                   'favs',)
+                   'date_joined',)

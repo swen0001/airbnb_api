a8ed7830bab49b056ecfed7fcfb0e1339991c5c8
swen0001 <oleksandr.venger0001@gmail.com>
2022-10-15 20:38:46 +0300

    Creating Account

--- users/serializers.py
+++ users/serializers.py
@@ -32,16 +32,31 @@ class ReadUserSerializer(serializers.ModelSerializer):
                    'date_joined',)
 
 
-class WriteUserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
         fields = (
+            'id',
             'username',
             'first_name',
             'last_name',
             'email',
+            'avatar',
+            'superhost',
+            'password',
         )
+        read_only_fields = ('id', 'superhost', 'avatar',)
 
     def validated_first_name(self, value):
         return value.upper()
+
+    def create(self, validated_data):
+        password = validated_data.get('password')
+        user = super().create(validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = "users"
 
 urlpatterns = [
+    path('', views.UserView.as_view()),
     path('me/', views.MeView.as_view()),
     path('me/favs/', views.FavsView.as_view()),
     path('<int:pk>/', views.user_detail),
